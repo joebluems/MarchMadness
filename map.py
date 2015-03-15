36b419a13214f9b44f80ecc24c60b831aa7e96a7
Joseph Blue <joebluems@yahoo.com>
2015-03-15 15:18:40 -0700

    Final version
    Including conference championship games through March 15, 2015.

--- map.py
+++ map.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python
 import csv
-import re
 import os
 
+#### create empty dictionaries ###
 count=0
 teams={}
 map={}
@@ -10,11 +9,12 @@ winSparse={}
 losses={}
 games={}
 
-with open('./games_mar12.csv','rb') as csv_file:
+##### read games file ####
+with open('./games_mar15.csv','rb') as csv_file:
     csv_reader = csv.reader(csv_file,delimiter='|')
-    for row in csv_reader:
-        ### remove rankings, create map of teams/integer ###
 
+    for row in csv_reader:
+        ### determine winner/loser ####
 	if int(row[1])>int(row[3]): 
 	   win=row[0]
 	   lose=row[2]
@@ -22,29 +22,31 @@ with open('./games_mar12.csv','rb') as csv_file:
 	   win=row[2]
 	   lose=row[0]
 	   
+        ### create map of teams/integer ###
         if win not in teams:
-	 teams[win]=count
-	 map[count]=win
-	 losses[count]=0
-	 games[count]=0
-	 count+=1
+	  teams[win]=count
+	  map[count]=win
+	  losses[count]=0
+	  games[count]=0
+	  count+=1
         if lose not in teams:
-	 teams[lose]=count
-	 map[count]=lose
-	 losses[count]=0
-	 games[count]=0
-	 count+=1
+	  teams[lose]=count
+	  map[count]=lose
+	  losses[count]=0
+	  games[count]=0
+	  count+=1
    
         value = 1
 	#### keep track of total games & losses for each team ###
         games[teams[lose]]+=1 
         games[teams[win]]+=1 
         losses[teams[lose]]+=value 
+
         ### create sparse matrix for each loser - increment number of wins ###
-        if teams[lose] not in winSparse: 
-	    winSparse[teams[lose]]={} 
+        if teams[lose] not in winSparse: winSparse[teams[lose]]={} 
         if teams[win] not in winSparse[teams[lose]]: winSparse[teams[lose]][teams[win]]=0
         winSparse[teams[lose]][teams[win]]+=value
+
 
 ##### print team mapping to file ####
 f = "teams"
@@ -57,12 +59,10 @@ for i in range(0,len(teams)):
       file.write("%s|%s|%s|%s\n" % (i,map[i],losses[i],games[i]))
 
 
-### print out H matrix ###
+##### print out dense H matrix to STDOUT #####
 for i in range(0,len(teams)):
-  #print i,map[i],losses[i],games[i]
   for j in range(0,len(teams)):
      if i not in winSparse: winSparse[i]={}
-     #if j in winSparse[i]: print "%f" % (winSparse[i][j]/float(losses[i])),
      if j in winSparse[i]: print "%f" % (float(winSparse[i][j])/max(25,losses[i])),
      else: print 0,
   print
